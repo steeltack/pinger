7bf07c3e700df775fe3c05d70520d65af93c77b6
KC Soderstrom <kc.soderstrom@moat.com>
2015-08-20 17:49:08 -0400

    omg do not commit merge conflicts

--- pinger.sh
+++ pinger.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-<<<<<<< HEAD
 alias clear='printf "\033c"'
 
 file_path="log.txt"
 
 clear
 echo "Pinger Initialized!"
-=======
-file_path="tester.txt"
->>>>>>> 6d1ed3220ffeb11ad72451f3d42130f0c3ac2e08
 
 while true; do
    sleep $[ ($RANDOM % 2640) + 60]s
